e48b377a383c2c6fde73c54a4ca60d38ec2c82cd
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-04-12 16:39:35 -0300

    Criação Category

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+}
