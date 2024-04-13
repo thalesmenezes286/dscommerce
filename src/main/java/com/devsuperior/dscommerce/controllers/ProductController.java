25d072a851cd8b8a7bd0427c76d9ef019a3c1e62
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-04-13 19:12:45 -0300

    feat: findAll Product

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -2,13 +2,13 @@ package com.devsuperior.dscommerce.controllers;
 
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,6 +21,14 @@ public class ProductController {
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 
         ProductDTO dto = productService.findById(id);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductMinDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
+
+        Page<ProductMinDTO> dto = productService.findAll(name, pageable);
 
         return ResponseEntity.ok(dto);
     }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,5 +25,13 @@ public class ProductService {
                 .orElseThrow(() -> new ResourceNotFoundException("RECURSO NÃO ENCONTRADO!"));
 
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
+
+        Page<Product> result = productRepository.searchByName(name, pageable);
+
+        return result.map(x -> new ProductMinDTO(x));
     }
 }
