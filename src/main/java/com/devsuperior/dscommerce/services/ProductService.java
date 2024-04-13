37a52a29742b1acbe9e7fe4839ae344fed2732ad
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-04-13 18:38:59 -0300

    feat: findById Product

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+
+        Product product = productRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("RECURSO NÃO ENCONTRADO!"));
+
+        return new ProductDTO(product);
+    }
+}
